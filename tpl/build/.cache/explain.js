843780cf1ff98a6e760b1d748cfc3349ed0f7768
wangsifang <wangsifang@huaxsoft.com>
2018-09-18 16:49:30 +0800

    更改 标题超出一行问题

--- tpl/build/.cache/explain.js
+++ tpl/build/.cache/explain.js
@@ -1,7 +1,7 @@
-/*TMODJS:{"version":14,"md5":"7f32405d3a36d2d0841741146937fea2"}*/
+/*TMODJS:{"version":15,"md5":"d462cdc13bad69a98f432c2d3b23c013"}*/
 template('explain',function($data,$filename
 /*``*/) {
-'use strict';var $utils=this,$helpers=$utils.$helpers,$escape=$utils.$escape,investigateTitle=$data.investigateTitle,$string=$utils.$string,description=$data.description,$out='';$out+='<div class="vote-title"> <h2 id="investigate-title">';
+'use strict';var $utils=this,$helpers=$utils.$helpers,$escape=$utils.$escape,investigateTitle=$data.investigateTitle,$string=$utils.$string,description=$data.description,$out='';$out+='<div class="vote-title"> <h2 id="investigate-title" class="ellipsis">';
 $out+=$escape(investigateTitle);
 $out+='</h2> </div> <div id="description" class="description"> <p>';
 $out+=$string(description);
