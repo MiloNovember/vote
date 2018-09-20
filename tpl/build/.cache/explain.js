217e6a826040baf56ba49b05ae9b2c1e9803b481
wangsifang <wangsifang@huaxsoft.com>
2018-09-20 17:55:33 +0800

    修改需求 与 bug

--- tpl/build/.cache/explain.js
+++ tpl/build/.cache/explain.js
@@ -1,4 +1,4 @@
-/*TMODJS:{"version":18,"md5":"5ff8207422f6edeab9c10423858b90d4"}*/
+/*TMODJS:{"version":20,"md5":"1d2205ff1a1e4395f15b596d955cfe97"}*/
 template('explain',function($data,$filename
 /*``*/) {
 'use strict';var $utils=this,$helpers=$utils.$helpers,$escape=$utils.$escape,investigateTitle=$data.investigateTitle,$string=$utils.$string,description=$data.description,$out='';$out+='<div class="vote-title"> <h2 id="investigate-title" class="ellipsis" title="';
@@ -7,6 +7,6 @@ $out+='">';
 $out+=$escape(investigateTitle);
 $out+='</h2> </div> <div id="description" class="description"> <p>';
 $out+=$string(description);
-$out+='</p> </div> <div class="line"></div>';
+$out+='</p> </div> <div class="line"></div> ';
 return new String($out);
 });
